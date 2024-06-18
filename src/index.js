72410887ccee0a1f914e5d4e4d7670897e54caa8
Elizabeth <itsalaz@icloud.com>
2024-06-18 09:01:22 -0400

    lab worked on

--- src/index.js
+++ src/index.js
@@ -1,55 +1,82 @@
 
+document.addEventListener('DOMContentLoaded', main)
+
+export {
+  main,
+  displayRamens,
+  handleClick,
+  submitListener,
+  addImage,
+}
+
+// handles the rendering of existing ramen display and display of new ramen 
+function main() {
+  displayRamens();
+  submitListener();
+}
+
+const ramenMenu = document.getElementById('ramen-menu')
+const detailImage = document.querySelector('.detail-image')
+const name = document.querySelector('.name')
+const restaurant= document.querySelector('.restaurant')
+const rating = document.getElementById('rating-display')
+const comment = document.getElementById('comment-display')
 
 
-function (main ('DOMContentLoaded', (displayRamens, addSubmitListener) => {
-  const displayRamens = () => {
-    let menu = document.querySelector('#ramen-menu') 
-    fetch('http://localhost:3000/ramens/ramen-menu')
-    .then(res => res.json())
-    .then(data => data.forEach(ramenImg) =>
-     )
 
+
+// requesting data from server and displaying ramen 
+function displayRamens() {
+  return fetch ('db.json')
+  .then(function(response) {
+    return response.json()
+  })
+  .then(data => {
+
+        data.ramens.forEach((ramen, index) => {
+        const img = document.createElement('img') // creating image with source display
+        img.src = ramen.image
+        img.alt = ramen.name
+        img.addEventListener('click', () => handleClick(ramen)) // moves ramen to main image upon click
+        ramenMenu.appendChild(img) 
+        if(index === 0) {  // sets first image as main image in initial ramen rendering
+          handleClick(ramen) 
+      }
+    })
+  })
+}
+
+// Linking HTML locational insert to ramen data to handleClick event 
+
+  function handleClick(ramen) {
+    detailImage.src = ramen.image
+    name.textContent = ramen.name 
+    restaurant.textContent = ramen.restaurant
+    rating.textContent = ramen.rating 
+    comment.textContent = ramen.comment
   }
 
-
-
-const handleClick = (ramen) => {
-  // Add code
-};
-
-const addSubmitListener = () => {
-  // Add code
-}
-
-
-const displayRamens = () => {
-  let menu = document.querySelector('#ramen-menu') 
-  fetch('http://localhost:3000/ramens/ramen-menu')
-  .then(res => res.json())
-  .then(data => console.log(data))
-
-}
-
-
-function initialize() {
-  displayRamens()
-}
-
-
-const main = () => {
-  // Invoke displayRamens here
-  // Invoke addSubmitListener here
-}
-
-main()
-
-// Export functions for testing
-export {
-  displayRamens,
-  addSubmitListener,
-  handleClick,
-  main,
-};
-
-
-}))
+// Targeting & submitting form element values for new-ramen
+  function submitListener() {
+    const form = document.querySelector('form')
+    form.addEventListener('click', (e) => {
+      e.preventDefault()
+      const newObject = {
+        name: e.target.name.value,
+        restaurant: e.target.restaurant.value,
+        image: e.target.image.value,
+        rating: e.target.rating.value,
+        comment: e.target["new-comment"].value, // new element not previously defined
+      }
+      addImage(newObj) // adds input from addImage function to click + submit 
+    })
+  }
+  
+// creates a new image after input from form values and appends it to ramenMenu display
+    function addImage(newObj) {
+      const img = document.createElement('img')
+        img.src = newObj.image 
+        img.alt = newObj.name
+        img.addEventListener('click', () => handleClick(newObj))
+        ramenMenu.appendChild(img)  
+    }
